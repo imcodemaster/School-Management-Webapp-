b8cfd974f72131bf0d118f7c8f5696833accf319
Vinay Bhatt <vinaybhatt0210@gmail.com>
2020-07-08 00:25:00 +0530

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cesa_chja4wvqqze(ubwu87lots6q8&g$jmkq^7+(yw7hv=(3^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'https://schoolmanagementwebapp.herokuapp.com/']
 
 
 # Application definition
